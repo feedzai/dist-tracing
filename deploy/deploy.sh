5e909b31567ca26b6559021e93fdd0d8db131003
GoncaloGarcia <GGFPC12495@gmail.com>
2019-07-23 13:08:30 +0100

    Update Docs

--- deploy/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ ! -z "$TRAVIS_TAG" ]; then
-    echo "TRAVIS TAG: $TRAVIS_TAG"
-    mvn deploy -Prelease --settings .m2/settings.xml -DskipTests=true
-    exit $?
-fi
