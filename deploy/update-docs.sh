5e804058a3e2c221aade910fa5949408f889f793
GoncaloGarcia <GGFPC12495@gmail.com>
2019-07-26 14:03:54 +0100

    Fix wrong paths in update-docs.sh

--- deploy/update-docs.sh
+++ deploy/update-docs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_PULL_REQUEST" = "false" ] && [ $(git diff github -- site/ | wc -l) -gt 0 ]];
+if [[ "$TRAVIS_PULL_REQUEST" = "false" && $(git diff github -- ../site/ | wc -l) -gt 0 ]];
 then
-	cd site/src/gitbook
+	cd ../site/src/gitbook
 	gitbook install
 	gitbook build
 	cd -
-	cp -R site/src/gitbook/_book/* docs/
-	git add docs/
+	cp -R ../site/src/gitbook/_book/* ../docs/
+	git add ../docs/
 	git commit -m "[TravisCI] Update Documentation"
 	git push github master
 fi
